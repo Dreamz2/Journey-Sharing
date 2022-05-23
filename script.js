6416438fa007a93ff9f50f41b11f3837bcb05801
Dreamz2 <ewei03@mylangara.ca>
2022-05-23 14:52:27 -0700

    Added fetch to Neew York api

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-var NY_token = config.NYTIME_TOKEN;
-var GOOGLE_TOKEN = config.GOOGLE_TOKEN;
+const NY_token = config.NYTIME_TOKEN;
+const GOOGLE_TOKEN = config.GOOGLE_TOKEN;
+
+function fillPageInitial() {
+    let BestSellers = document.querySelector("#BestSellers_Content");
+    let message = '<p>Hello</p>';
+
+    document.querySelector("#BestSellers_Content").innerHTML += message;
+
+    const data1 = fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key='+ NY_token)
+        .then(response => response.json())
+        .then(data => console.log(data));
+
+    
+    
+}
+fillPageInitial();
