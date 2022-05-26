35610ba81426dec381e58d3841f031b541595412
Dreamz2 <ewei03@mylangara.ca>
2022-05-26 00:40:18 -0700

    implemented connection with NYT Books API and Created javascrpit containing popular genres

---
+++ Genres.js
@@ -0,0 +1,51 @@
+var genres = {
+    Genres: {
+        Popular_Genres: {
+            Fiction:{
+                Fantasy: "Fantasy",
+                Science_Fiction: "Science Fiction",
+                Dystopian: "Dystopian",
+                Action_and_Adventure: "Action & Adventure",
+                Mystery: "Mystery",
+                Horror: "Horror",
+                Thriller_and_Suspense: "Thriller & Suspense",
+                Historical_Fiction: "Historical Fiction",
+                Romance: "Romance",
+                Womens_Fiction: "Womens Fiction",
+                LGBTQ: "LGBTQ+",
+                Contemporary_Fiction: "Contemporary Fiction",
+                Literary_Fiction: "Literary Fiction",
+                Magical_Realism: "Magical Realism",
+                Graphic_Novel: "Graphic Novel",
+                Short_Story: "Short Story",
+                Young_Adult: "Young Adult",
+                Childrens: "Children's",
+            },
+            Non_Fiction: {
+                Memoir_and_Autobiography: "Memoir & Autobiography",
+                Biography: "Biography",
+                Food_and_Drink: "Food & Drink",
+                Art_and_Photography: "Art & Photography",
+                Self_help: "Self help",
+                History: "History",
+                Travel: "Travel",
+                True_Crime: "True Crime",
+                Humor: "Humor",
+                Essays: "Essays",
+                Guides: "Guides",
+                How_to: "How to",
+                Religion_and_Spirituality: "Religion & Spirituality",
+                Humanities_and_Social_Sciences: "Humanities & Social Sciences",
+                Parenting_and_Families: "Parenting & Families",
+                Science_and_Technology: "Science & Technology",
+                Childrens: "Children’s"
+            }
+        },
+        Non_Popular_Genres:{
+
+        }
+    },
+    Sub_Genres:{
+
+    }
+};
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const NY_token = config.NYTIME_TOKEN;
 const GOOGLE_TOKEN = config.GOOGLE_TOKEN;
+const data = genres;
+
+let BestSellers_Content = document.getElementById(`BestSellers_Content`);
 
 async function fillPageInitial(NY_url) {
     // var BestSellers = document.querySelector("#BestSellers_Content");
@@ -7,20 +10,49 @@ async function fillPageInitial(NY_url) {
 
     // document.querySelector("#BestSellers_Content").innerHTML += message;
 
-    var BestSellers = await BestSellers_data(NY_url);
-    var LastGenre = await LastGenre();
-    var Fiction = await Fiction();
-    var nonFiction = await nonFiciton();
+    //var BestSellers = await BestSellers_data(NY_url);
+    // var test =;
+    // var LastGenre = await LastGenre();
+    // var Fiction = await Fiction();
+    // var nonFiction = await nonFiciton();
 
-    console.log(BestSellers);
-    console.log(BestSellers.results.books[0]);
+    //console.log(BestSellers);
+    console.log(data);
+    console.log(data.Genres.Popular_Genres.Fiction.Test);
     
-    
+    // let displayed = 0;
+    // while(displayed!=5){
+    //     buildBestSellers(BestSellers, displayed);
+    //     displayed++;
+    // }
 }
-fillPageInitial('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key='+ NY_token);
+//fillPageInitial('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key='+ NY_token);
+fillPageInitial(`https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=${NY_token}`);
 
 async function BestSellers_data(NY_url) {
-    var response = await fetch(NY_url)
+    var response = await fetch(NY_url);
+        // .then(response => {
+        //     if(!response.ok){
+        //         throw new Error('Error');
+        //     }
+        //     return response.blob;
+        // });
+
 
     return await response.json();
+}
+
+function buildBestSellers(data, current){
+    BestSellers_Content = `<div class="Book_Title_Author">
+                                <div class="Book_Cover">
+                                    <img src="${data.results.lists[current].books[current].book_image}" 
+                                    alt="${data.results.lists[current].books[current].description}"
+                                    width="${data.results.lists[current].books[current].book_image_width}" 
+                                    height="${data.results.lists[current].books[current].book_image_height}">
+                                </div>
+                                <p id="Book_Title">${data.results.lists[current].books[current].title}</p>
+                                <p id="Book_Author">${data.results.lists[current].books[current].author}</p>
+                            </div>`;
+
+    document.querySelector("#BestSellers_Content").innerHTML += BestSellers_Content;
 }
